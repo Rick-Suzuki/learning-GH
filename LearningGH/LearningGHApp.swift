574453eb2ed24aabc7dd59d207e93d1b7ef9e495
Rick01 <mindtn2@gmail.com>
2021-10-13 13:34:50 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by rick on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
